6250e6b38015be25727862c670b559220fad7db1
Januarius93 <marstyczen@gmail.com>
2025-03-28 18:44:03 +0100

    test updated for order create controller

--- order_compose_machine/src/test/java/org/restaurant/order_compose_machine/controller/order/CreateOrderControllerTest.java
+++ order_compose_machine/src/test/java/org/restaurant/order_compose_machine/controller/order/CreateOrderControllerTest.java
@@ -1,44 +1,76 @@
 package org.restaurant.order_compose_machine.controller.order;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import java.net.URI;
 import java.util.List;
 import lombok.SneakyThrows;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.restaurant.order_compose_machine.AbstractUnitTest;
 import org.restaurant.order_compose_machine.OCMUnitTest;
+import org.restaurant.order_compose_machine.config.ApiResponse;
 import org.restaurant.order_compose_machine.controller.OrderController;
 import org.restaurant.order_compose_machine.dto.ProductDto;
 import org.restaurant.order_compose_machine.dto.order.OrderDto;
+import org.restaurant.order_compose_machine.dto.order.OrderMapper;
 import org.restaurant.order_compose_machine.dto.order_item.OrderItemDto;
+import org.restaurant.order_compose_machine.model.order.Order;
+import org.restaurant.order_compose_machine.model.order.OrderItem;
+import org.restaurant.order_compose_machine.model.product.Product;
+import org.restaurant.order_compose_machine.repository.OrderRepository;
 import org.restaurant.order_compose_machine.service.OrderServiceImpl;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-@Import(OrderController.class)
+@ExtendWith(MockitoExtension.class)
 public class CreateOrderControllerTest extends AbstractUnitTest implements OCMUnitTest {
 
   @Mock private OrderServiceImpl orderService;
+  @InjectMocks private OrderController orderController;
+  @Mock private OrderRepository orderRepository;
+  @Mock private OrderMapper orderMapper;
 
   @SneakyThrows
   @Test
   public void withProperOrderPayloadOrderCanBeCreatedWithHttp200() {
-    OrderDto orderDto =
-        OrderDto.builder()
-            .listOfOrderItems(
-                List.of(OrderItemDto.builder().product(ProductDto.builder().build()).build()))
-            .build();
+    Long orderId = 1L;
+
+    Order order1 = new Order();
+    order1.setOrderId(orderId);
+
+    OrderItem orderItem = new OrderItem();
+    orderItem.setItemName("Pizza");
+
+    Product product = new Product();
+    product.setProductName("Pizza Product");
+
+    orderItem.setProduct(product);
+    order1.setListOfOrderItems(List.of(orderItem));
+
+    OrderDto orderDto = new OrderDto();
+    orderDto.setOrderId(orderId);
+
+    OrderItemDto orderItemDto = new OrderItemDto();
+    orderItemDto.setItemName("Pizza");
+
+    ProductDto productDto = new ProductDto();
+    productDto.setProductName("Pizza Product");
+
+    orderItemDto.setProduct(productDto);
+    orderDto.setListOfOrderItems(List.of(orderItemDto));
+
     when(orderService.createOrder(orderDto)).thenReturn(orderDto);
 
-    ResultActions response = sendMockedPostRequest(new URI("/api/order/createOrder"), orderDto);
-    response
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(jsonPath("$.message").value("Order created"));
+    ResponseEntity<ApiResponse<OrderDto>> responseOrder = orderController.createOrder(orderDto);
+
+    assertThat(responseOrder, notNullValue());
+    assertThat(responseOrder.getStatusCode(), Matchers.is(HttpStatus.OK));
+    assertThat(responseOrder.getBody().getData().getOrderId(), Matchers.is(orderId));
   }
 }
