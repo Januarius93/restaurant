82ea740b2c503293d4eaa19950a8f980a1864a74
Januarius93 <marstycze@gmail.com>
2025-05-05 16:53:06 +0200

    order state updated

---
+++ menu-svc/src/main/java/com/restaurant/menu_svc/MenuSvcApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.menu_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuSvcApplication.class, args);
+	}
+
+}
---
+++ menu-svc/src/test/java/com/restaurant/menu_svc/MenuSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.menu_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
