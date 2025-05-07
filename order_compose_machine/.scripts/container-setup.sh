b913860c2bf02931f609772e6ee262560a302993
Marcin <marstyczen@gmail.com>
2025-05-07 20:15:54 +0200

    scripts updated (#15)
    Co-authored-by: Januarius93 <marstycze@gmail.com>

--- order_compose_machine/.scripts/container-setup.sh
+++ order_compose_machine/.scripts/container-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Building Docker image..."
+echo "Building Docker order-compose-machine image..."
 docker build -t order-compose-machine .
 if [ $? -ne 0 ]; then
     echo "Failed to build the Docker image."
     exit 1
 fi
 
-echo "Running Docker container..."
+echo "Running Docker order-compose-machine container..."
 docker run -p 9090:9090 order-compose-machine
 if [ $? -ne 0 ]; then
     echo "Failed to run the Docker container."
     exit 1
 fi
 
-echo "Docker container started successfully."
+echo "Docker order-compose-machine container started successfully."
